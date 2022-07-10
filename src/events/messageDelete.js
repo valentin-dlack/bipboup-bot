46af7f57bac054ce8541cacfd9e7dfad6f4b144b
valentin dautrement <phuss.pro@gmail.com>
2022-07-10 17:30:18 +0200

    fix msgupdate, msgdelete & msgdelete bulk

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -18,29 +18,30 @@ module.exports = {
     async execute(message, client) {
         conn.query(`SELECT * FROM OPTIONS WHERE guild_id = ${message.guild.id}`, (err, rows) => {
             if (err) throw err;
+            if (rows.length > 0) {
+                if (rows[0].logDel == true) {
+                    let logChannel = message.guild.channels.cache.find(channel => channel.id === rows[0].log_channel);
+                    if (!logChannel) return;
 
-            if (rows[0].logDel == true) {
-                let logChannel = message.guild.channels.cache.find(channel => channel.id === rows[0].log_channel);
-                if (!logChannel) return;
+                    const attachment = message.attachments.size ? message.attachments.map(attachment => attachment.proxyURL) : null;
 
-                const attachment = message.attachments.size ? message.attachments.map(attachment => attachment.proxyURL) : null;
+                    let timestamp = moment(message.createdAt).format('DD/MM/YYYY HH:mm:ss');
 
-                let timestamp = moment(message.createdAt).format('DD/MM/YYYY HH:mm:ss');
+                    if (message.content.length > 800) {
+                        message.content = message.content.substring(0, 800) + '...';
+                    }
 
-                if (message.content.length > 800) {
-                    message.content = message.content.substring(0, 800) + '...';
+                    let logEmbed = new MessageEmbed()
+                        .setColor('#ff0000')
+                        .setTitle('Message Logs :')
+                        .addField(`Informations :`, `\`\`\`diff\n▶ Message supprimé dans : ${message.channel.name}\n▶ Ecrit par : ${message.author.tag}\n\`\`\``)
+                        .addField(`Timestamp :`, `<t:${Math.floor(Date.now()/1000)}>`)
+                        .addField(`Contenu du message :`, `\`\`\`${message.cleanContent ? message.cleanContent : "[Empty message]"}\`\`\``)
+                        .addField(`Attachements :`, `${attachment ? attachment.join('\n') : "No Attachements"}`)
+                        .setFooter({ text: `ID du message : ${message.id}`, iconUrl: message.author.displayAvatarURL({ format: 'png' }) });
+
+                    logChannel.send({ embeds: [logEmbed] });
                 }
-
-                let logEmbed = new MessageEmbed()
-                    .setColor('#ff0000')
-                    .setTitle('Message Logs :')
-                    .addField(`Informations :`, `\`\`\`diff\n▶ Message supprimé dans : ${message.channel.name}\n▶ Ecrit par : ${message.author.tag}\n\`\`\``)
-                    .addField(`Timestamp :`, `<t:${Math.floor(Date.now()/1000)}>`)
-                    .addField(`Contenu du message :`, `\`\`\`${message.cleanContent ? message.cleanContent : "[Empty message]"}\`\`\``)
-                    .addField(`Attachements :`, `${attachment ? attachment.join('\n') : "No Attachements"}`)
-                    .setFooter({ text: `ID du message : ${message.id}`, iconUrl: message.author.displayAvatarURL({ format: 'png' }) });
-
-                logChannel.send({ embeds: [logEmbed] });
             }
         });
     }
--- src/events/messageDeleteBulk.js
+++ src/events/messageDeleteBulk.js
@@ -18,26 +18,27 @@ module.exports = {
     async execute(messages, client) {
         conn.query(`SELECT * FROM OPTIONS WHERE guild_id = ${messages.first().guild.id}`, (err, rows) => {
             if (err) throw err;
+            if (rows.length > 0) {
+                if (rows[0].logDel == true) {
+                    let logChannel = messages.first().guild.channels.cache.find(channel => channel.id === rows[0].log_channel);
+                    if (!logChannel) return;
 
-            if (rows[0].logDel == true) {
-                let logChannel = messages.first().guild.channels.cache.find(channel => channel.id === rows[0].log_channel);
-                if (!logChannel) return;
+                    let message_count = 0;
 
-                let message_count = 0;
+                    messages.forEach(msg => {
+                        message_count++;
+                    });
 
-                messages.forEach(msg => {
-                    message_count++;
-                });
+                    let logEmbed = new MessageEmbed()
+                        .setColor('#ff0000')
+                        .setTitle('Message Logs :')
+                        .addField(`Informations :`, `▶ Messages supprimés dans : <#${messages.first().channel.id}>`)
+                        .addField(`Nombre de messages supprimés:`, `\`${message_count}\``)
+                        .addField(`Timestamp :`, `<t:${Math.floor(Date.now()/1000)}>`)
+                        .setFooter({ text: `Bulk Delete`, iconUrl: client.user.displayAvatarURL({ format: 'png' }) });
 
-                let logEmbed = new MessageEmbed()
-                    .setColor('#ff0000')
-                    .setTitle('Message Logs :')
-                    .addField(`Informations :`, `▶ Messages supprimés dans : <#${messages.first().channel.id}>`)
-                    .addField(`Nombre de messages supprimés:`, `\`${message_count}\``)
-                    .addField(`Timestamp :`, `<t:${Math.floor(Date.now()/1000)}>`)
-                    .setFooter({ text: `Bulk Delete`, iconUrl: client.user.displayAvatarURL({ format: 'png' }) });
-
-                logChannel.send({ embeds: [logEmbed] });
+                    logChannel.send({ embeds: [logEmbed] });
+                }
             }
         });
     }
