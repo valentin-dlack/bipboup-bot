2a32057bb60356edaa683ff4ca005d5e6d8dc4a5
valentin dautrement <phuss.pro@gmail.com>
2022-09-29 01:00:25 +0200

    secret feature

---
+++ src/events/messageCreate.js
@@ -0,0 +1,13 @@
+const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
+const cfg = require('../../cfg.json');
+const mysql = require('mysql');
+
+module.exports = {
+    name: 'messageCreate',
+    once: false,
+
+    async execute(message, client) {
+        if (message.guild.id !== "659289330010816525") return;
+        if (message.content.toLowerCase().endsWith("bambou")) return message.reply("la !");
+    }
+}
